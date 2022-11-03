e8ce355ae941b7ec1f170e64bc68708c41d3c1c5
obaidmuneer <obaidmuneer@gmail.com>
2022-11-03 23:09:47 +0500

    added geo location

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import axios from 'axios';
 import { useState, useEffect } from 'react';
-import './App.css';
+import axios from 'axios';
+
 import Weather from './components/weather';
+import './App.css';
 
 function App() {
-  const [city, setCity] = useState('Karachi')
+  const [city, setCity] = useState('')
   const [weatherData, setWeatherData] = useState({})
-
+  // const [geo, setGeo] = useState('')
+  // const [data, setData] = useState('')
   useEffect(() => {
-    fethcedData()
+    if (window.navigator.geolocation) {
+      const successfulLookup = position => {
+        let { latitude, longitude } = position.coords;
+        fethcedData(latitude, longitude)
+      }
+
+      window.navigator.geolocation
+        .getCurrentPosition(successfulLookup, console.log);
+    }
   }, [])
 
   let getData = (e) => {
@@ -16,12 +26,21 @@ function App() {
     fethcedData()
   }
 
-  let fethcedData = () => {
+  let fethcedData = (lat, lon) => {
+    let data;
     let apiKey = '55ff5b9f1aa556e25d9767c01329b185'
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+    if (lat && lon) {
+      data = `lat=${lat}&lon=${lon}`
+    } else {
+      let city = document.querySelector('#city').value
+      data = `q=${city}`
+    }
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?${data}&appid=${apiKey}&units=metric`)
       .then((res) => {
+        console.log(res.data.weather);
         setWeatherData({
-          name : res.data.name,
+          name: res.data.name,
+          icon: res.data.weather[0].icon,
           date: Date.now(),
           temp: Math.round(res.data.main.temp)
         })
@@ -29,18 +48,32 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="App-header">
-        <form onSubmit={getData} >
-          <span>Enter City Name: </span>
-          <input type="text" onChange={e => setCity(e.target.value)} value={city} />
-          <button type="submit">Get weather</button>
-        </form>
-        <Weather
-          city={weatherData.name}
-          date={weatherData.date}
-          temp={weatherData.temp}
-        />
+    <div >
+      <div >
+        <div className="container">
+
+          <div className="demo-flex-spacer"></div>
+
+          <form onSubmit={getData}>
+            <div className="webflow-style-input">
+              <input type="text" placeholder="Enter your city name!" onChange={e => setCity(e.target.value)} value={city} />
+              <button type="submit"><i className="icon ion-android-arrow-forward"></i>{'>'}</button>
+
+            </div>
+          </form>
+          {
+            weatherData?.name && <Weather
+            city={weatherData.name}
+            date={weatherData.date}
+            temp={weatherData.temp}
+            icon={weatherData.icon}
+          />
+          }
+          
+
+          <div className="demo-flex-spacer"></div>
+        </div>
+
       </div>
     </div>
   );
